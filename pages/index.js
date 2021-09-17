495aaf4ea1fe3d109a2b795d08245a185e1ddadb
VIII4 <iyan.laylor@gmail.com>
2021-09-17 14:55:21 -0500

    added mobile banner and reponsive styling

--- pages/index.js
+++ pages/index.js
@@ -3,11 +3,13 @@ import styles from "../styles/Home.module.css";
 
 import Layout from "../components/layout";
 import FullScreenIntro from "../components/panels/fullscreenIntro";
+import MobileBanner from "../components/elements/mobileBanner";
 
 export default function Home() {
   return (
     <Layout>
       <FullScreenIntro />
+      <MobileBanner />
       <main className={styles.main}>
         <h1 className={styles.title}>
           Coming Soon <a href="https://nextjs.org">Next.js!</a> OVERHAUL
