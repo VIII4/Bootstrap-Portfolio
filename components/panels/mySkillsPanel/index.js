a98c2f119ac97340658143143deb862abb214bdd
VIII4 <iyan.laylor@gmail.com>
2021-09-28 14:50:46 -0500

    added skills panel and list

--- components/panels/mySkillsPanel/index.js
+++ components/panels/mySkillsPanel/index.js
@@ -1,4 +1,11 @@
-import { MDBContainer, MDBRow, MDBCol, MDBTypography } from "mdb-react-ui-kit";
+import {
+  MDBContainer,
+  MDBRow,
+  MDBCol,
+  MDBTypography,
+  MDBListGroup,
+  MDBListGroupItem,
+} from "mdb-react-ui-kit";
 import React, { useState } from "react";
 import Image from "next/image";
 import styles from "./mySkillsPanel.module.css";
@@ -6,31 +13,53 @@ import styles from "./mySkillsPanel.module.css";
 export function MySkillsPanel() {
   //
   return (
-    <MDBContainer fluid className={`${styles.skillsBackground} `}>
-      <MDBRow className="text-center p-1">
-        <MDBTypography tag="h4" className="text-muted ">
-          Designer
-        </MDBTypography>
+    <MDBContainer fluid className={`${styles.skillsBackground}`}>
+      <MDBRow center className={styles.skillsHeader}>
+        <MDBCol className="text-center ">
+          <MDBTypography tag="h4" className="text-muted ">
+            Design
+          </MDBTypography>
+        </MDBCol>
       </MDBRow>
-      <MDBRow className={styles.skillsWrapper}>
-        <MDBCol /* TO DO: STYLE To make element shift aboove background */
-          className=""
+      <MDBRow center className="mb-2 ">
+        <MDBCol
+          size="12"
+          md="8"
+          lg="8"
+          className="rounded bg-light text-center p-2"
         >
-          <MDBRow className="rounded bg-light text-center p-">
-            <MDBTypography tag="h4" className="text-muted m-0">
-              Designer List...
-            </MDBTypography>
-          </MDBRow>
-          <MDBRow center className="my-2">
-            <MDBTypography tag="h4" className="text-muted m-0 text-center">
-              Developer
-            </MDBTypography>
-          </MDBRow>
-          <MDBRow className="rounded bg-light text-center p-">
-            <MDBTypography tag="h4" className="text-muted m-0">
-              Designer List...
-            </MDBTypography>
-          </MDBRow>
+          <MDBListGroup flush>
+            <MDBListGroupItem>Cras justo odio</MDBListGroupItem>
+            <MDBListGroupItem>Dapibus ac facilisis in</MDBListGroupItem>
+            <MDBListGroupItem>Vestibulum at eros</MDBListGroupItem>
+            <MDBListGroupItem>Cras justo odio</MDBListGroupItem>
+            <MDBListGroupItem>Dapibus ac facilisis in</MDBListGroupItem>
+            <MDBListGroupItem>Vestibulum at eros</MDBListGroupItem>
+          </MDBListGroup>
+        </MDBCol>
+      </MDBRow>
+      <MDBRow center className={styles.skillsHeader}>
+        <MDBCol className="text-center">
+          <MDBTypography tag="h4" className="text-muted ">
+            Development
+          </MDBTypography>
+        </MDBCol>
+      </MDBRow>
+      <MDBRow center className="mb-5 ">
+        <MDBCol
+          size="12"
+          md="8"
+          lg="8"
+          className="rounded bg-light text-center p-3"
+        >
+          <MDBListGroup flush>
+            <MDBListGroupItem>Cras justo odio</MDBListGroupItem>
+            <MDBListGroupItem>Dapibus ac facilisis in</MDBListGroupItem>
+            <MDBListGroupItem>Vestibulum at eros</MDBListGroupItem>
+            <MDBListGroupItem>Cras justo odio</MDBListGroupItem>
+            <MDBListGroupItem>Dapibus ac facilisis in</MDBListGroupItem>
+            <MDBListGroupItem>Vestibulum at eros</MDBListGroupItem>
+          </MDBListGroup>
         </MDBCol>
       </MDBRow>
     </MDBContainer>
